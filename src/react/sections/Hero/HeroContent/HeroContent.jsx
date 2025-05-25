a9efebef3b08b3c503f5d9ac0c098471ba391fff
Kristina Potapenko <potapenkokristina10@gmail.com>
2025-05-25 18:18:33 +0300

    Create HeroContent component

---
+++ src/react/sections/Hero/HeroContent/HeroContent.jsx
@@ -0,0 +1,23 @@
+import { Button } from "../../../components/ui/Button/Button";
+import styles from "./HeroContent.module.scss";
+
+export const HeroContent = () => {
+  return (
+    <div className={styles.content}>
+      <h1 className={styles.title}>The Best Streaming Experience</h1>
+      <p className={styles.description}>
+        StreamVibe is the best streaming experience for watching your favorite
+        movies and shows on demand, anytime, anywhere. With StreamVibe, you can
+        enjoy a wide variety of content, including the latest blockbusters,
+        classic movies, popular TV shows, and more. You can also create your own
+        watchlists, so you can easily find the content you want to watch.
+      </p>
+      <Button type="button">
+        <svg className="icon">
+          <use xlinkHref="assets/icon/sprite.svg#play" />
+        </svg>
+        Start Watching Now
+      </Button>
+    </div>
+  );
+};
