6ea87e1c1f6800d415960e44c2801d462c96d29d
Kristina Potapenko <potapenkokristina10@gmail.com>
2025-05-26 21:22:51 +0300

    Update page home

--- src/react/pages/Home/Home.jsx
+++ src/react/pages/Home/Home.jsx
@@ -1,9 +1,11 @@
-import React from "react";
 import { Hero } from "../../sections/Hero/Hero";
+import { Devices } from "../../sections/Devices/Devices";
+
 export const Home = () => {
   return (
     <>
       <Hero />
+      <Devices />
     </>
   );
 };
