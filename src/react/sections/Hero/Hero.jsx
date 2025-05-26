a44ff1797c6f88eea35c7bcda51368e58370836e
Kristina Potapenko <potapenkokristina10@gmail.com>
2025-05-26 21:07:56 +0300

    Update style with image

--- src/react/sections/Hero/Hero.jsx
+++ src/react/sections/Hero/Hero.jsx
@@ -1,9 +1,11 @@
 import { HeroContent } from "./HeroContent/HeroContent";
 import styles from "./Hero.module.scss";
+import { Link } from "react-router-dom";
 
 export const Hero = () => {
   return (
     <div className={styles.hero}>
+      <Link className={styles.play} />
       <section className="container">
         <HeroContent />
       </section>
