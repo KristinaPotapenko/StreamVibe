6750d1072057378ddbb19d02db4778b3c489bdc1
Kristina Potapenko <potapenkokristina10@gmail.com>
2025-05-27 20:57:13 +0300

    Add adaptive for Social component

--- src/react/components/ui/Social/Social.jsx
+++ src/react/components/ui/Social/Social.jsx
@@ -4,19 +4,33 @@ export const Social = () => {
   return (
     <ul className={styles.list}>
       <li className={styles.item}>
-        <svg className="icon">
-          <use xlinkHref="/assets/icon/sprite.svg#facebook" />
-        </svg>
+        <a
+          href="https://www.facebook.com/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <svg className="icon">
+            <use xlinkHref="/assets/icon/sprite.svg#facebook" />
+          </svg>
+        </a>
       </li>
       <li className={styles.item}>
-        <svg className="icon">
-          <use xlinkHref="/assets/icon/sprite.svg#twitter" />
-        </svg>
+        <a href="https://x.com/" target="_blank" rel="noopener noreferrer">
+          <svg className="icon">
+            <use xlinkHref="/assets/icon/sprite.svg#twitter" />
+          </svg>
+        </a>
       </li>
       <li className={styles.item}>
-        <svg className="icon">
-          <use xlinkHref="/assets/icon/sprite.svg#linkedin" />
-        </svg>
+        <a
+          href="https://ua.linkedin.com/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <svg className="icon">
+            <use xlinkHref="/assets/icon/sprite.svg#linkedin" />
+          </svg>
+        </a>
       </li>
     </ul>
   );
