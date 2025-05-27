c1087dc6ed902426d3010d7923c5c476c75ce62b
Kristina Potapenko <potapenkokristina10@gmail.com>
2025-05-27 20:47:34 +0300

    Add hook for mobile menu

---
+++ src/scripts/hook/useNoScroll.js
@@ -0,0 +1,15 @@
+import { useEffect } from "react";
+
+export const useNoScroll = (isActive) => {
+  useEffect(() => {
+    if (isActive) {
+      document.body.classList.add("no-scroll");
+    } else {
+      document.body.classList.remove("no-scroll");
+    }
+
+    return () => {
+      document.body.classList.remove("no-scroll");
+    };
+  }, [isActive]);
+};
