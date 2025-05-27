c6412ad0d44314c6114708bb5464af75b39bfd2b
Kristina Potapenko <potapenkokristina10@gmail.com>
2025-05-27 20:42:10 +0300

    Add adaptive for header

--- src/react/components/layouts/Header/Header.jsx
+++ src/react/components/layouts/Header/Header.jsx
@@ -1,16 +1,37 @@
+import { useState } from "react";
 import { NavigationMenu } from "../../ui/NavigationMenu/NavigationMenu";
 import { Actions } from "../../Actions/Actions";
 import { Logo } from "../../ui/Logo/Logo";
+import { useNoScroll } from "../../../../scripts/hook/useNoScroll";
 import styles from "./Header.module.scss";
 
 export const Header = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  useNoScroll(isOpen);
+
+  const handleChangeOpen = (isOpen = false) => {
+    setIsOpen(isOpen);
+  };
+
   return (
-    <section className="container">
-      <header className={styles.header}>
+    <header className={styles.header}>
+      <section className="container">
         <Logo />
-        <NavigationMenu />
-        <Actions />
-      </header>
-    </section>
+        <div className={`${styles.links} ${isOpen ? styles.isActive : ""}`}>
+          <NavigationMenu onClick={handleChangeOpen} />
+          <Actions onClick={handleChangeOpen} />
+          <button onClick={() => setIsOpen()} className={styles.closeButton}>
+            <svg className="icon">
+              <use xlinkHref="/assets/icon/sprite.svg#close" />
+            </svg>
+          </button>
+        </div>
+        <button onClick={() => setIsOpen(true)} className={styles.burgerButton}>
+          <svg className="icon">
+            <use xlinkHref="/assets/icon/sprite.svg#burger" />
+          </svg>
+        </button>
+      </section>
+    </header>
   );
 };
