7b23ef7e98f667c5f550f9b6434cd6b76769fb96
Kristina Potapenko <potapenkokristina10@gmail.com>
2025-05-27 20:59:49 +0300

    Add adaptive for Hero section

--- src/react/sections/Hero/Hero.jsx
+++ src/react/sections/Hero/Hero.jsx
@@ -1,11 +1,11 @@
 import { HeroContent } from "./HeroContent/HeroContent";
-import styles from "./Hero.module.scss";
 import { Link } from "react-router-dom";
+import styles from "./Hero.module.scss";
 
 export const Hero = () => {
   return (
     <div className={styles.hero}>
-      <Link className={styles.play} />
+      <Link to="/movies-tv" className={styles.play} />
       <section className="container">
         <HeroContent />
       </section>
