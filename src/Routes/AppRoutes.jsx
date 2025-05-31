7c6feb4608ae51933cb6990cf330fb39347da2d1
Kristina Potapenko <potapenkokristina10@gmail.com>
2025-05-31 21:41:01 +0300

    Add route to AppRoutes

--- src/Routes/AppRoutes.jsx
+++ src/Routes/AppRoutes.jsx
@@ -1,10 +1,13 @@
 import { Route, Routes } from "react-router-dom";
 import { Home } from "../react/pages/Home/Home";
+import { ROUTES } from "../utils/routes";
+import { MoviesAndTV } from "../react/pages/MoviesAndTV/MoviesAndTV";
 
 export const AppRoutes = () => {
   return (
     <Routes>
       <Route index element={<Home />} />
+      <Route path={ROUTES.MOVIES_TV} element={<MoviesAndTV />} />
     </Routes>
   );
 };
