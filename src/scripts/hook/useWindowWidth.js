b860e8ec3b3247c6aea1d0e370771370072fbbc4
Kristina Potapenko <potapenkokristina10@gmail.com>
2025-05-31 21:30:26 +0300

    refactoring code for hook useWindowWidth

--- src/scripts/hook/useWindowWidth.js
+++ src/scripts/hook/useWindowWidth.js
@@ -2,7 +2,6 @@ import { useEffect, useState } from "react";
 
 export const useWindowWidth = () => {
   const [width, setWidth] = useState(window.innerWidth);
-  console.log(width);
 
   useEffect(() => {
     const handleResize = () => setWidth(window.innerWidth);
