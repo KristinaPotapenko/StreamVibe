3e90e1b7f236bb1c99f91daae613bf3c1f053ac0
Kristina Potapenko <potapenkokristina10@gmail.com>
2025-05-31 21:37:18 +0300

    Create topRatedMovies reducer and add to store

--- src/features/store.js
+++ src/features/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import categoriesReducer from "./categories/categoriesSlice";
+import topMoviesReducer from "./topRatedMovies/topMoviesReducerSlice";
 
 export const store = configureStore({
   reducer: {
     categoriesList: categoriesReducer,
+    topMovies: topMoviesReducer,
   },
 });
