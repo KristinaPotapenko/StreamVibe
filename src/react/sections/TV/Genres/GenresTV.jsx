3eeef6b541be8e6cdd7d86ae000e9bff29010aa6
Kristina Potapenko <potapenkokristina10@gmail.com>
2025-06-03 21:44:51 +0300

    Create GenresTV component

---
+++ src/react/sections/TV/Genres/GenresTV.jsx
@@ -0,0 +1,23 @@
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getGenresTV } from "../../../../features/tv/genresTV/genresTVSlice";
+import { genresTVImage } from "./genresTVImage";
+import { Slider } from "../../../components/Slider/Slider/Slider";
+
+export const GenresTV = () => {
+  const dispatch = useDispatch();
+  const { genresTV } = useSelector(({ genresTV }) => genresTV);
+
+  useEffect(() => {
+    dispatch(getGenresTV());
+  }, [dispatch]);
+
+  return (
+    <Slider
+      title="Our Genres"
+      list={genresTV}
+      image={genresTVImage}
+      footer="genres"
+    />
+  );
+};
---
+++ src/react/sections/TV/Genres/genresTVImage.js
@@ -0,0 +1,35 @@
+import Action from "../../../../assets/genresTV/action.jpg?react";
+import Animation from "../../../../assets/genresTV/animation.jpg?react";
+import Comedy from "../../../../assets/genresTV/comedy.jpg?react";
+import Crime from "../../../../assets/genresTV/crime.png?react";
+import Documentary from "../../../../assets/genresTV/documentary.jpg?react";
+import Drama from "../../../../assets/genresTV/drama.jpeg?react";
+import Family from "../../../../assets/genresTV/family.jpg?react";
+import Kids from "../../../../assets/genresTV/kids.jpg?react";
+import Mystery from "../../../../assets/genresTV/mystery.jpg?react";
+import News from "../../../../assets/genresTV/news.jpg?react";
+import Reality from "../../../../assets/genresTV/reality.jpg?react";
+import Fantasy from "../../../../assets/genresTV/fantasy.jpg?react";
+import Soap from "../../../../assets/genresTV/soap.jpg?react";
+import Talk from "../../../../assets/genresTV/talk.jpg?react";
+import War from "../../../../assets/genresTV/war.jpg?react";
+import Western from "../../../../assets/genresTV/western.jpg?react";
+
+export const genresTVImage = [
+  { id: 10759, img: Action },
+  { id: 16, img: Animation },
+  { id: 35, img: Comedy },
+  { id: 80, img: Crime },
+  { id: 99, img: Documentary },
+  { id: 18, img: Drama },
+  { id: 10751, img: Family },
+  { id: 10762, img: Kids },
+  { id: 9648, img: Mystery },
+  { id: 10763, img: News },
+  { id: 10764, img: Reality },
+  { id: 10765, img: Fantasy },
+  { id: 10766, img: Soap },
+  { id: 10767, img: Talk },
+  { id: 10768, img: War },
+  { id: 37, img: Western },
+];
