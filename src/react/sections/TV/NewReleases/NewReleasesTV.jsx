0a80f6f53fa1d468e130ee86e1a6478525dc2c36
Kristina Potapenko <potapenkokristina10@gmail.com>
2025-06-03 21:47:42 +0300

    Create NewReleasesTV component

---
+++ src/react/sections/TV/NewReleases/NewReleasesTV.jsx
@@ -0,0 +1,19 @@
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getNewRealeasesTV } from "../../../../features/tv/newReleasesTV/newReleasesTVSlice";
+import { Slider } from "../../../components/Slider/Slider/Slider";
+
+export const NewReleasesTV = () => {
+  const dispatch = useDispatch();
+  const { newRealeasesTV } = useSelector(
+    ({ newRealeasesTV }) => newRealeasesTV
+  );
+
+  useEffect(() => {
+    dispatch(getNewRealeasesTV());
+  }, [dispatch]);
+
+  return (
+    <Slider title="New Releases" list={newRealeasesTV} footer="releases" />
+  );
+};
