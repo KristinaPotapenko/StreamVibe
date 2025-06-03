c4a721cd7d12be5a8ffcdbb6ca06f6650118012e
Kristina Potapenko <potapenkokristina10@gmail.com>
2025-06-03 21:50:36 +0300

    Add TV component and tabs

--- src/react/pages/MoviesAndTV/MoviesAndTV.jsx
+++ src/react/pages/MoviesAndTV/MoviesAndTV.jsx
@@ -1,11 +1,52 @@
+import { useState } from "react";
+import { Tabs } from "../../components/ui/Tabs/Tabs";
 import { Movies } from "../../sections/Movies/Movies";
 import { TopMovies } from "../../sections/TopMovies/TopMovies";
+import { TV } from "../../sections/TV/TV";
+import { useWindowWidth } from "../../../scripts/hook/useWindowWidth";
+import styles from "./MoviesAndTV.module.scss";
 
 export const MoviesAndTV = () => {
+  const [activeTabs, setActiveTabs] = useState(0);
+  const windowWidth = useWindowWidth();
+
+  const isMobile = windowWidth < 768;
+  const isDesktop = windowWidth >= 768;
+
   return (
     <>
       <TopMovies />
-      <Movies />
+      {isMobile && (
+        <>
+          <Tabs
+            tabs={["Movies", "Shows"]}
+            activeTabs={activeTabs}
+            setActiveTabs={setActiveTabs}
+          />
+          <div className={styles.tabsContent}>
+            <div
+              className={`${styles.tabPanel} ${
+                activeTabs === 0 ? styles.active : ""
+              }`}
+            >
+              <Movies />
+            </div>
+            <div
+              className={`${styles.tabPanel} ${
+                activeTabs === 1 ? styles.active : ""
+              }`}
+            >
+              <TV />
+            </div>
+          </div>
+        </>
+      )}
+      {isDesktop && (
+        <>
+          <Movies />
+          <TV />
+        </>
+      )}
     </>
   );
 };
