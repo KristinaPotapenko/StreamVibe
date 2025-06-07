2fc1ce0c30b1c18453e0071cf24d5ec9f8722916
Kristina Potapenko <potapenkokristina10@gmail.com>
2025-06-07 12:03:39 +0300

    Add anchor link to Questions component

--- src/react/sections/Questions/Questions.jsx
+++ src/react/sections/Questions/Questions.jsx
@@ -1,11 +1,11 @@
 import { ROUTES } from "../../../utils/routes";
-import { SectionHeader } from "../../components/Section/SectionHeader/sectionHeader";
 import { Link } from "../../components/ui/Link/Link";
+import { SectionHeader } from "../../components/Section/SectionHeader/sectionHeader";
 import { QuestionsList } from "./QuestionsList/QuestionsList";
 
 export const Questions = () => {
   return (
-    <section className="section container">
+    <section id="questions" className="section container">
       <SectionHeader
         title="Frequently Asked Questions"
         description="Got questions? We've got answers! Check out our FAQ section to find answers to the most common questions about StreamVibe."
