e747d1ea8511c3168abb7ba568f05bc1c158bea3
Kristina Potapenko <potapenkokristina10@gmail.com>
2025-06-07 12:02:26 +0300

    Add anchor link to Devices component

--- src/react/sections/Devices/Devices.jsx
+++ src/react/sections/Devices/Devices.jsx
@@ -3,7 +3,7 @@ import { DevicesList } from "./DevicesList/DevicesList";
 
 export const Devices = () => {
   return (
-    <section className="section container">
+    <section id="devices" className="section container">
       <SectionHeader
         title="We Provide you streaming experience across various devices."
         description="With StreamVibe, you can enjoy your favorite movies and TV shows anytime, anywhere. Our platform is designed to be compatible with a wide range of devices, ensuring that you never miss a moment of entertainment."
