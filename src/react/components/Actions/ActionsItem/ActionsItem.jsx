6d34243816a93a55424fb687a0d9ae6c758f0147
Kristina Potapenko <potapenkokristina10@gmail.com>
2025-06-07 12:16:32 +0300

    Update Actions component, make more universal

--- src/react/components/Actions/ActionsItem/ActionsItem.jsx
+++ src/react/components/Actions/ActionsItem/ActionsItem.jsx
@@ -2,14 +2,30 @@ import { Link } from "react-router-dom";
 import { ROUTES } from "../../../../utils/routes";
 import styles from "./ActionsItem.module.scss";
 
-export const ActionsItem = ({ accent, href, route, onClick }) => {
+export const ActionsItem = ({
+  type = "link",
+  accent,
+  href,
+  route,
+  onClick,
+}) => {
   return (
     <li className={`${styles.actionsItem} ${accent ? styles.accent : ""}`}>
-      <Link to={route ? ROUTES[route] : ""} onClick={() => onClick()}>
-        <svg className="icon">
-          <use xlinkHref={`/assets/icon/sprite.svg#${href}`} />
-        </svg>
-      </Link>
+      {type === "link" && (
+        <Link to={route ? ROUTES[route] : ""} onClick={() => onClick()}>
+          <svg className="icon">
+            <use xlinkHref={`/assets/icon/sprite.svg#${href}`} />
+          </svg>
+        </Link>
+      )}
+      {type === "button" && (
+        <button onClick={() => onClick()}>
+          {" "}
+          <svg className="icon">
+            <use xlinkHref={`/assets/icon/sprite.svg#${href}`} />
+          </svg>
+        </button>
+      )}
     </li>
   );
 };
