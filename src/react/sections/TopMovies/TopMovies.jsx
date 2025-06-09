6c6e28f8ddd334913ba7550b14da4637aee991bb
Kristina Potapenko <potapenkokristina10@gmail.com>
2025-06-09 12:46:09 +0300

    fix optional chaining for backdrop_path access

--- src/react/sections/TopMovies/TopMovies.jsx
+++ src/react/sections/TopMovies/TopMovies.jsx
@@ -45,7 +45,7 @@ export const TopMovies = () => {
         className={styles.sliderBg}
         style={{
           backgroundImage: `linear-gradient(0deg,rgba(0, 0, 0, 1) 16%, rgba(20, 20, 20, 0.26) 100%), url(${
-            topMovies[activeSlide].backdrop_path
+            topMovies[activeSlide]?.backdrop_path
               ? baseImageURL + topMovies[activeSlide]?.backdrop_path
               : "https://img.freepik.com/premium-vector/gallery-simple-icon-vector-image-picture-sign-neumorphism-style-mobile-app-web-ui-vector-eps-10_532800-801.jpg"
           })`,
