7e71bf3c751e4b995b10afea4c40d611181d6fda
Kristina Potapenko <potapenkokristina10@gmail.com>
2025-06-16 21:50:09 +0300

    Add logic for show season tv when press Play Now button in SeasonItem component

--- src/react/sections/MediaInfo/MediaBlocks/SeasonItem/SeasonItem.jsx
+++ src/react/sections/MediaInfo/MediaBlocks/SeasonItem/SeasonItem.jsx
@@ -1,25 +1,51 @@
-import { useState } from "react";
-import { formattingDate } from "../../../../../scripts/helpers/formattingDate";
+import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useParams } from "react-router-dom";
 import { baseImageURL, FALLBACK_IMAGE } from "../../../../../utils/constants";
+import { getTvSeasonVideo } from "../../../../../features/tv/tvSeasonVideo/tvSeasonVideoSlice";
+import { formattingDate } from "../../../../../scripts/helpers/formattingDate";
+import { Button } from "../../../../components/ui/Button/Button";
 import { Rate } from "../../../../components/Rate/Rate";
 import { SliderButton } from "../../../../components/Slider/SliderButton/SliderButton";
 import styles from "../../MediaInfo.module.scss";
 import stylesSeason from "./SeasonItem.module.scss";
 
 export const SeasonItem = ({ season }) => {
+  const dispatch = useDispatch();
+  const { tvId } = useParams();
   const [isOpenDetails, setIsOpenDetails] = useState(false);
+  const [showVideo, setShowVideo] = useState(false);
+  const videoKeyObj = useSelector(
+    ({ tvSeasonVideo }) => tvSeasonVideo.seasonVideos
+  );
 
   const openDetails = () => {
     setIsOpenDetails(!isOpenDetails);
   };
 
-  return (
+  useEffect(() => {
+    showVideo &&
+      dispatch(getTvSeasonVideo({ tvId, seasonId: season.season_number + 1 }));
+  }, [showVideo]);
+
+  return showVideo ? (
+    <iframe
+      width="100%"
+      height="500"
+      src={`https://www.youtube.com/embed/${
+        videoKeyObj[season.season_number + 1]
+      }`}
+      title="Video player"
+      frameBorder="0"
+      allowFullScreen
+    ></iframe>
+  ) : (
     <li
       className={`${stylesSeason.season} ${
         isOpenDetails ? stylesSeason.open : ""
       }`}
     >
-      <div className={styles.infoHeader}>
+      <div className={`${styles.infoHeader} ${stylesSeason.infoHeader}`}>
         <div className={stylesSeason.info}>
           <p className={styles.name}>{season.name}</p>
           <p className={styles.episodes}>{season.episode_count} Episodes</p>
@@ -27,14 +53,16 @@ export const SeasonItem = ({ season }) => {
         <SliderButton onClick={openDetails} isGray={true} />
       </div>
       <div className={stylesSeason.details}>
-        <img
-          src={`${
-            season?.poster_path
-              ? baseImageURL + season?.poster_path
-              : FALLBACK_IMAGE
-          }`}
-          alt={season?.name}
-        />
+        <div>
+          <img
+            src={`${
+              season?.poster_path
+                ? baseImageURL + season?.poster_path
+                : FALLBACK_IMAGE
+            }`}
+            alt={season?.name}
+          />
+        </div>
         <div className={stylesSeason.statistics}>
           <div className={styles.info}>
             <div className={styles.infoName}>
@@ -65,6 +93,18 @@ export const SeasonItem = ({ season }) => {
                 </p>
               </li>
             </ul>
+          </div>
+          <div className={styles.info}>
+            <Button
+              onClick={() => {
+                setShowVideo(true);
+              }}
+            >
+              <svg className="icon">
+                <use xlinkHref="/assets/icon/sprite.svg#play" />
+              </svg>
+              Play Now
+            </Button>
           </div>
         </div>
       </div>
