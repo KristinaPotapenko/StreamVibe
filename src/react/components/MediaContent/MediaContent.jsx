1a366bb30a5dfd1ceec7c57aab28b5deeefdd7e4
Kristina Potapenko <potapenkokristina10@gmail.com>
2025-06-16 21:45:37 +0300

    Add logic for show movie when press Play Now button in Media component

--- src/react/components/MediaContent/MediaContent.jsx
+++ src/react/components/MediaContent/MediaContent.jsx
@@ -1,32 +1,68 @@
-import { ROUTES } from "../../../utils/routes";
-import { getRouteWithId } from "../../../scripts/helpers/getRouteWithId";
-import { Link } from "../ui/Link/Link";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useParams } from "react-router-dom";
+import { getMovieVideo } from "../../../features/movies/movieVideo/movieVideoSlice";
+import { scrollToTop } from "../../../scripts/helpers/scrollToTop";
+import { Button } from "../ui/Button/Button";
 import { Actions } from "../Actions/Actions";
 import { ActionsItem } from "../Actions/ActionsItem/ActionsItem";
 import { SectionTitle } from "../Section/SectionTitle/SectionTitle";
 import styles from "./MediaContent.module.scss";
 
-export const MediaContent = ({ media, isMovie }) => {
-  return (
+export const MediaContent = ({
+  media,
+  activeSlide,
+  showVideo,
+  setShowVideo,
+  isMovie,
+  isTopMovie,
+}) => {
+  const dispatch = useDispatch();
+
+  const { movieId } = useParams();
+  const topMovieId = media[activeSlide]?.id;
+
+  const videoKey = useSelector(({ movieVideo }) => movieVideo.movieKey);
+
+  useEffect(() => {
+    showVideo && dispatch(getMovieVideo(isTopMovie ? topMovieId : movieId));
+  }, [showVideo]);
+
+  return showVideo ? (
+    <div className={styles.content}>
+      <iframe
+        width="100%"
+        height="500"
+        src={`https://www.youtube.com/embed/${videoKey}`}
+        title="Video player"
+        frameBorder="0"
+        allowFullScreen
+      ></iframe>
+    </div>
+  ) : (
     <div className={styles.content}>
       <SectionTitle
         title={
-          media.original_title ? media.original_title : media?.original_name
+          media[activeSlide]?.original_title
+            ? media[activeSlide]?.original_title
+            : media[activeSlide]?.original_name
         }
       />
-      <p className={styles.description}>{media?.overview}</p>
+      <p className={styles.description}>{media[activeSlide]?.overview}</p>
       <div className={styles.actions}>
-        <Link
-          to={getRouteWithId(
-            isMovie ? ROUTES.MOVIE_DETAILS : ROUTES.TV_DETAILS,
-            media?.id
-          )}
-        >
-          <svg className="icon">
-            <use xlinkHref="/assets/icon/sprite.svg#play" />
-          </svg>
-          Play Now
-        </Link>
+        {isMovie && (
+          <Button
+            onClick={() => {
+              setShowVideo(true);
+              scrollToTop();
+            }}
+          >
+            <svg className="icon">
+              <use xlinkHref="/assets/icon/sprite.svg#play" />
+            </svg>
+            Play Now
+          </Button>
+        )}
         <Actions>
           <ActionsItem type="button" accent={true} href="plus" />
           <ActionsItem type="button" accent={true} href="like" />
--- src/react/sections/Preview/Preview.jsx
+++ src/react/sections/Preview/Preview.jsx
@@ -1,26 +1,37 @@
+import { useState } from "react";
 import { baseImageURL, FALLBACK_IMAGE } from "../../../utils/constants";
 import { MediaContent } from "../../components/MediaContent/MediaContent";
 import styles from "./Preview.module.scss";
 
 export const Preview = ({ isFirstSection = false, media, isMovie }) => {
+  const [showVideo, setShowVideo] = useState(false);
+
   return (
     <section
-      className={`section container ${isFirstSection ? "first-section" : ""} ${
-        styles.section
-      }`}
+      className={`section container ${isFirstSection ? "first-section" : ""} 
+     `}
     >
       <div
-        className={styles.sliderBg}
-        style={{
-          backgroundImage: `linear-gradient(0deg,rgba(0, 0, 0, 1) 16%, rgba(20, 20, 20, 0.26) 100%), url(${
-            media.backdrop_path
-              ? baseImageURL + media?.backdrop_path
-              : FALLBACK_IMAGE
-          })`,
-        }}
+        className={showVideo ? undefined : styles.sliderBg}
+        style={
+          showVideo
+            ? undefined
+            : {
+                backgroundImage: `linear-gradient(0deg,rgba(0, 0, 0, 1) 16%, rgba(20, 20, 20, 0.26) 100%), url(${
+                  media.backdrop_path
+                    ? baseImageURL + media?.backdrop_path
+                    : FALLBACK_IMAGE
+                })`,
+              }
+        }
       >
-        <div className={styles.sliderContent}>
-          <MediaContent media={media} isMovie={isMovie} />
+        <div className={showVideo ? undefined : styles.sliderContent}>
+          <MediaContent
+            media={media}
+            showVideo={showVideo}
+            setShowVideo={setShowVideo}
+            isMovie={isMovie}
+          />
         </div>
       </div>
     </section>
