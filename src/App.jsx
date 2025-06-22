75017e58ede5c5f4dfc018f1099370c1d458233c
Kristina Potapenko <potapenkokristina10@gmail.com>
2025-06-22 10:45:31 +0300

    Add ModalWelcome to App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,27 @@
+import { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import { AppRoutes } from "./Routes/AppRoutes";
 import { Footer } from "./react/components/layouts/Footer/Footer";
 import { Header } from "./react/components/layouts/Header/Header";
-import { AppRoutes } from "./Routes/AppRoutes";
+import { ModalWelcome } from "./react/components/ui/ModalWelcome/ModalWelcome";
 
 function App() {
+  const accountType = useSelector(
+    ({ userAuthentication }) => userAuthentication.accountType
+  );
+  const [showModal, setShowModal] = useState(false);
+
+  useEffect(() => {
+    if (accountType === null) {
+      setShowModal(true);
+    } else {
+      setShowModal(false);
+    }
+  }, [accountType]);
+
   return (
     <div className="wrapper">
+      {showModal && <ModalWelcome setShowModal={setShowModal} />}
       <Header />
       <main>
         <AppRoutes />
