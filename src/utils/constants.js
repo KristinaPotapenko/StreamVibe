631d4d3d97740aa669684c7f77be924dc0a97f1a
Kristina Potapenko <potapenkokristina10@gmail.com>
2025-06-22 10:33:25 +0300

    Update API_KEY constant

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -4,4 +4,5 @@ export const baseImageURL = `https://image.tmdb.org/t/p/w500/`;
 export const FALLBACK_IMAGE =
   "https://img.freepik.com/premium-vector/gallery-simple-icon-vector-image-picture-sign-neumorphism-style-mobile-app-web-ui-vector-eps-10_532800-801.jpg";
 
-export const API_KEY = "${API_KEY}";
+export const API_KEY =
+  "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJlZDM1ZDZlMDUzOWQ2M2FmODA3NGZmZTgzMzg2MjlmNyIsIm5iZiI6MTc0Nzk5OTA4MC40NDgsInN1YiI6IjY4MzA1OTY4MmQ1ZDVmYmNmYjA1NDQ3MyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.xBdgADr7TleB5hyfUvuFJuOWwQq3gUYeJCq6jlY9wuM";
