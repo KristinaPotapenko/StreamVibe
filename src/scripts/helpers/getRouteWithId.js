4558882e73065f36a31c4f82575b97a7ddd7b3fe
Kristina Potapenko <potapenkokristina10@gmail.com>
2025-06-22 20:21:16 +0300

    Add helpers function getRouteWithParams

--- src/scripts/helpers/getRouteWithId.js
+++ src/scripts/helpers/getRouteWithId.js
@@ -1,3 +1,9 @@
 export const getRouteWithId = (route, id) => {
   return route.replace(/:\w+Id/, id);
 };
+
+export const getRouteWithParams = (route, params) => {
+  return Object.entries(params).reduce((result, [key, value]) => {
+    return result.replace(`:${key}`, value);
+  }, route);
+};
