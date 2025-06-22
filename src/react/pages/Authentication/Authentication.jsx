dddd1e585f001b592f430ff797cf09b42cec0b29
Kristina Potapenko <potapenkokristina10@gmail.com>
2025-06-22 10:40:13 +0300

    Create Authentication component

---
+++ src/react/pages/Authentication/Authentication.jsx
@@ -0,0 +1,42 @@
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Navigate, useNavigate } from "react-router-dom";
+import {
+  getSessionId,
+  setAccountType,
+} from "../../../features/authentication/userAuthenticationSlice";
+import { ROUTES } from "../../../utils/routes";
+
+export const Authentication = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const sessionId = useSelector(
+    ({ userAuthentication }) => userAuthentication.sessionId
+  );
+
+  const requestToken = new URLSearchParams(window.location.search).get(
+    "request_token"
+  );
+
+  useEffect(() => {
+    if (requestToken) {
+      dispatch(getSessionId(requestToken));
+    }
+  }, [dispatch, requestToken]);
+
+  useEffect(() => {
+    if (sessionId) {
+      dispatch(setAccountType("user"));
+      localStorage.setItem("accountType", "user");
+      localStorage.setItem("sessionId", sessionId);
+      navigate(ROUTES.HOME);
+    }
+  }, [sessionId, navigate]);
+
+  if (!requestToken) {
+    return <Navigate to={ROUTES.HOME} replace />;
+  }
+
+  return <div>Loading...</div>;
+};
