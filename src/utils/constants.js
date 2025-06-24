6ca4b3d5a06d45b17e65ccb53ca6e8347c4fde25
Kristina Potapenko <potapenkokristina10@gmail.com>
2025-06-24 14:39:24 +0300

    Add constant account_id

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -6,3 +6,5 @@ export const FALLBACK_IMAGE =
 
 export const API_KEY =
   "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJlZDM1ZDZlMDUzOWQ2M2FmODA3NGZmZTgzMzg2MjlmNyIsIm5iZiI6MTc0Nzk5OTA4MC40NDgsInN1YiI6IjY4MzA1OTY4MmQ1ZDVmYmNmYjA1NDQ3MyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.xBdgADr7TleB5hyfUvuFJuOWwQq3gUYeJCq6jlY9wuM";
+
+export const account_id = "22029052";
